dc5be09976d41a0794b205eba5ee4c6e09887bfa
Roshelle Moats <roshelle.moats@gmail.com>
2023-07-17 19:07:53 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=)ll)k75c3gbgb-x1nntge%b=qt%h9s51=6+#bw*1ljvqz$-i5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
